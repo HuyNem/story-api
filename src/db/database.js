e39342e37390dad8b2be42bcdcc937106044f211
Ngoc Huy <daogiahuy2002@gmail.com>
2024-07-04 09:07:33 +0700

    update database

--- src/db/database.js
+++ src/db/database.js
@@ -6,10 +6,10 @@ const mongoose = require("mongoose");
 
 async function connect() {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/story');
-    // await mongoose.connect(
-    //   "mongodb+srv://daogiahuy2002:huyliem2002@cluster0.a9rwhtm.mongodb.net/storyDB?retryWrites=true&w=majority&appName=Cluster0"
-    // );
+    // await mongoose.connect('mongodb://127.0.0.1:27017/story');
+    await mongoose.connect(
+      "mongodb+srv://daogiahuy2002:huyliem2002@cluster0.a9rwhtm.mongodb.net/storyDB?retryWrites=true&w=majority&appName=Cluster0"
+    );
     console.log("Kết nối database thành công");
   } catch (error) {
     console.log("Kết nối database không thành công", error);
